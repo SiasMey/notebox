caf363922342067772d7a798f6834a4585c49fdd
Sias Mey <siamey@gmail.com>
2024-02-03 11:12:00 +0200

    chore: set output binary name

--- ci/main.go
+++ ci/main.go
@@ -17,6 +17,10 @@ func main() {
 func build(ctx context.Context) error {
 	fmt.Println("Building with Dagger")
 
+	// define build matrix
+	oses := []string{"linux", "darwin"}
+	arches := []string{"amd64", "arm64"}
+
 	// initialize Dagger client
 	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
 	if err != nil {
@@ -27,25 +31,38 @@ func build(ctx context.Context) error {
 	// get reference to the local project
 	src := client.Host().Directory(".")
 
+	// create empty directory to put build outputs
+	outputs := client.Directory()
+
 	// get `golang` image
 	golang := client.Container().From("golang:latest")
 
 	// mount cloned repository into `golang` image
 	golang = golang.WithDirectory("/src", src).WithWorkdir("/src")
 
-	// define the application build command
-	path := "build/"
-	golang = golang.WithExec([]string{"go", "build", "-o", path, "cmd/nbx/main.go"})
+	for _, goos := range oses {
+		for _, goarch := range arches {
+			// create a directory for each os and arch
+			path := fmt.Sprintf("build/%s/%s/", goos, goarch)
 
-	// get reference to build output directory in container
-	output := golang.Directory(path)
+			// set GOARCH and GOOS in the build environment
+			build := golang.WithEnvVariable("GOOS", goos)
+			build = build.WithEnvVariable("GOARCH", goarch)
 
-	// write contents of container build/ directory to the host
-	_, err = output.Export(ctx, path)
+			binary := fmt.Sprintf("%s/%s", path, "nbx")
+
+			// build application
+			build = build.WithExec([]string{"go", "build", "-o", binary, "cmd/nbx/main.go"})
+
+			// get reference to build output directory in container
+			outputs = outputs.WithDirectory(path, build.Directory(path))
+		}
+	}
+	// write build artifacts to host
+	_, err = outputs.Export(ctx, ".")
 	if err != nil {
 		return err
 	}
-	defer client.Close()
 
 	return nil
 }
