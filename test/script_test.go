5b14879bbea2334f991ccd1b86b2d8504e5bcdb5
Sias Mey <siasmey@gmail.com>
2024-02-21 20:42:36 +0200

    test: add script tests to execute cli

---
+++ test/script_test.go
@@ -0,0 +1,13 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/rogpeppe/go-internal/testscript"
+)
+
+func Test(t *testing.T) {
+	testscript.Run(t, testscript.Params{
+		Dir: "testdata",
+	})
+}
