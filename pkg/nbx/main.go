c28246bdb8c7c4c1543836be3dccd90fbcbc8b8a
Sias Mey <siasmey@gmail.com>
2024-03-09 15:40:44 +0200

    feat: update usage dialogue

--- pkg/nbx/main.go
+++ pkg/nbx/main.go
@@ -5,11 +5,17 @@ import (
 	"os"
 )
 
+const usage = `usage: nbx [subcommand]
+
+subcommands:
+  tags: lists all tags in notebox`
+
 func Main() int {
 	if len(os.Args[1:]) < 1 {
-		fmt.Fprintln(os.Stderr, "usage: hello NAME")
+		fmt.Fprintln(os.Stderr, usage)
 		return 1
 	}
+
 	fmt.Println("Hello to you,", os.Args[1])
 	return 0
 }
