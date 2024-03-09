b63cbc1e2b78bc20497c52f1df9b558962bbce5a
Sias Mey <siasmey@gmail.com>
2024-03-09 15:27:47 +0200

    chore: rename test script

--- pkg/nbx/main.go
+++ pkg/nbx/main.go
@@ -1,8 +1,15 @@
 package nbx
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func Main() int {
-	fmt.Println("Hello, World")
+	if len(os.Args[1:]) < 1 {
+		fmt.Fprintln(os.Stderr, "usage: hello NAME")
+		return 1
+	}
+	fmt.Println("Hello to you,", os.Args[1])
 	return 0
 }
