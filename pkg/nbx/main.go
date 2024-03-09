32e3e6f021cb1d7a990f5ad5afe3a8620f565bde
Sias Mey <siasmey@gmail.com>
2024-03-09 16:10:51 +0200

    refactor: extract regex match

--- pkg/nbx/main.go
+++ pkg/nbx/main.go
@@ -26,8 +26,12 @@ func Main() int {
 	}
 
 	content_str := string(content)
-	for _, tag := range tag_pattern.FindAllString(content_str, -1) {
+	for _, tag := range getTagsFromFile(content_str) {
 		fmt.Fprintln(os.Stdout, strings.Trim(tag, "#"))
 	}
 	return 0
+}
+
+func getTagsFromFile(content_str string) []string {
+	return tag_pattern.FindAllString(content_str, -1)
 }
